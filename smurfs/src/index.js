5fbecb3ca61c47e3acb9a054c352463fee34f79b
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-13 11:28:00 -0500

    finished setting up index created base reducer, added some dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer(logger))
+import {reducer} from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+    <Provider store = {store} >
+        <App />
+    </Provider>
+, document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    smurf: {},
+    isFetchcing: false,
+    error: '',
+    newSmurf: [
+        {
+
+        }
+    ]
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
