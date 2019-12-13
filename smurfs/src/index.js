1a1c91aa0e0615310c8eb83f8ba6d1700bca182d
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-13 11:14:11 -0500

    created base indexjs dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+
+const store = createStore(reducer(logger))
 
 ReactDOM.render(<App />, document.getElementById("root"));
